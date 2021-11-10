e81896f259c136a43896ed162cff9522fc06442e
Manuel Wegener <m.wegener94@gmx.de>
2021-11-10 09:37:02 +0000

    done3

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log(res);
+  //console.log(res);
   res.sendFile(__dirname + "/public/views/index.html");
 });
 
 app.get("/works", (req, res) => {
-  console.log(res);
+  //console.log(res);
   res.sendFile(__dirname + "/public/views/works.html");
 });
 
 app.get("/about", (req, res) => {
-  console.log(res);
+  //console.log(res);
   res.sendFile(__dirname + "/public/views/about.html");
 });
 
 app.get("/gallery", (req, res) => {
-  console.log(res);
+  //console.log(res);
   res.sendFile(__dirname + "/public/views/gallery.html");
 });
 
