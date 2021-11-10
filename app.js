6f75ce829d6244ece5c2d9556d1c36daa15e1cfb
Manuel Wegener <m.wegener94@gmx.de>
2021-11-10 09:54:46 +0000

    done4

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get("/gallery", (req, res) => {
   res.sendFile(__dirname + "/public/views/gallery.html");
 });
 
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/views/404.html");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
